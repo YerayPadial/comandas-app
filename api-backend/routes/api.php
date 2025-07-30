ad140dc1284bb77c351f90eb8e2caaddd6b86dfb
YerayPadial <yeraykeira@gmail.com>
2025-07-30 13:00:07 +0200

    V.0.2. Modelos, migraciones, rutas, controllers-base backend

--- api-backend/routes/api.php
+++ api-backend/routes/api.php
@@ -1,4 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComandaController;
+use App\Http\Controllers\ProductoController;
 
 Route::get('/ping', fn() => response()->json(['pong' => true]));
+Route::apiResource('comandas', ComandaController::class)->only(['index', 'store', 'update']);
+Route::apiResource('productos', ProductoController::class)->except(['show']);
