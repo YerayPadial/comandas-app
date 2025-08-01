323f2cdc449fba3d9bc24866b67c79a49b35b0c8
YerayPadial <yeraykeira@gmail.com>
2025-08-01 13:32:20 +0200

    V.0.6.1 Actualizado frontend camareros folders

--- frontend-camareros/src/app/app.routes.ts
+++ frontend-camareros/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
+import { MesaComponent } from './features/mesa/mesa/mesa.component';
+import { ComandaComponent } from './features/comanda/comanda/comanda.component';
+import { PedidosComponent } from './features/pedidos/pedidos/pedidos.component';
+import { ProductosComponent } from './features/productos/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'mesa', component: MesaComponent },
+    { path: 'comanda', component: ComandaComponent },
+    { path: 'pedidos', component: PedidosComponent },
+    { path: 'productos', component: ProductosComponent },
+];
---
+++ frontend-camareros/src/app/features/comanda/comanda/comanda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comanda',
+  imports: [],
+  templateUrl: './comanda.component.html',
+  styleUrl: './comanda.component.css'
+})
+export class ComandaComponent {
+
+}
