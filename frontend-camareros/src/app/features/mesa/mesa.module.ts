b63f00216e04864ee9272480aa2e032c789510c2
YerayPadial <yeraykeira@gmail.com>
2025-08-02 15:17:19 +0200

    V.1.1. Productos camareros

--- frontend-camareros/src/app/features/mesa/mesa.module.ts
+++ frontend-camareros/src/app/features/mesa/mesa.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MesaComponent } from './mesa/mesa.component';
 
 
 
 @NgModule({
-  declarations: [],
+  exports: [MesaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MesaComponent
   ]
 })
 export class MesaModule { }
--- frontend-camareros/src/app/features/mesa/mesa/mesa.component.ts
+++ frontend-camareros/src/app/features/mesa/mesa/mesa.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductoService } from '../../../core/services/producto.service';
+import { Producto } from '../../../core/models/producto.model';
+import { CurrencyPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mesa',
-  imports: [],
+  standalone: true,
   templateUrl: './mesa.component.html',
-  styleUrl: './mesa.component.css'
+  imports: [NgFor, NgIf, NgClass, CurrencyPipe, FormsModule],
 })
-export class MesaComponent {
+export class MesaComponent implements OnInit {
+  nombreMesa = '';
+  productos: Producto[] = [];
+  categorias: string[] = [];
+  categoriaSeleccionada = '';
+  comandaActual: { [id: number]: { producto: Producto, cantidad: number } } = {};
 
+  loading = signal(true);
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productoService: ProductoService
+  ) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.nombreMesa = params['nombre'] || '';
+    });
+    this.productoService.getProductos().subscribe(productos => {
+      this.productos = productos;
+      this.categorias = Array.from(new Set(productos.map(p => p.categoria)));
+      this.categoriaSeleccionada = this.categorias[0] || '';
+      this.loading.set(false);
+    });
+  }
+
+  agregarProducto(producto: Producto) {
+    if (this.comandaActual[producto.id]) {
+      this.comandaActual[producto.id].cantidad++;
+    } else {
+      this.comandaActual[producto.id] = { producto, cantidad: 1 };
+    }
+  }
+
+  verComanda() {
+    localStorage.setItem('comanda', JSON.stringify({
+      nombreMesa: this.nombreMesa,
+      productos: Object.values(this.comandaActual)
+    }));
+    this.router.navigate(['/comanda']);
+  }
+
+  get productosFiltrados(): Producto[] {
+    return this.productos.filter(p => p.categoria === this.categoriaSeleccionada);
+  }
+
+  get tieneComanda(): boolean {
+    return Object.keys(this.comandaActual).length > 0;
+  }
 }
